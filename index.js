fa8bb85179af9c4369e873771b3e1856dbcd1618
Dino Dunvjak <dinoduvnjak@gmail.com>
2018-06-11 02:06:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
 }
 
 function deleteFromObjectByKey(obj,key) {
-  var newObj = Object.assign({}, {[key] });
+  var newObj = Object.assign({}, obj);
   delete newObj.key
   return newObj
 }
