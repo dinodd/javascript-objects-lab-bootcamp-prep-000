9dc9309b90c90f13b117bfa5fe14fbf582cd51d6
Dino Dunvjak <dinoduvnjak@gmail.com>
2018-06-11 01:40:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ function Objects() {
 function updateObjectWithKeyAndValue(obj,key,value) {
   return Object.assign({}, obj, {[key]: value});
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  return obj[key] = value
+}
